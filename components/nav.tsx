e87f0254c24e17073df7c57176b5e64d7f307fbc
Aykut Kardaş <aykutkrds@gmail.com>
2024-03-06 15:27:39 +0300

    fix: responsive

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,7 @@
 import { ThemeToggle } from "@/components/theme-toggle";
-import { ModelIcon } from "@/components/icons/model-icon";
 import Link from "next/link";
 import { Space_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { GithubIcon } from "lucide-react";
 
 const spaceMono = Space_Mono({
@@ -17,9 +15,11 @@ export function Nav() {
     <div className="h-14 py-2 px-2 md:px-8 border-b flex items-center">
       <div className="flex flex-1 items-center">
         <Link href="/">
-          <h1 className={cn("font-light text-xl", spaceMono.className)}>
+          <h1
+            className={cn("font-light text-sm md:text-lg", spaceMono.className)}
+          >
             <span className="text-pink-700">upscaler</span>
-            <span>⚔️</span>
+            <span className="mx-1">⚔️</span>
             <span>battleground</span>
           </h1>
         </Link>
